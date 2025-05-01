34ec37c77a7d3cf20e491aa8b2e5cf79c718ed65
wanshicheng <wanshicheng@qq.com>
2025-05-01 22:18:13 +0800

    Update main_wasm.go

--- main_wasm.go
+++ main_wasm.go
@@ -11,27 +11,13 @@ func logToJS(message string) {
 	js.Global().Get("console").Call("log", "[Go]:", message)
 }
 
-// 注册一个函数，用于从JS传递字符串到Go并返回，测试中文编码
-func registerStringEcho() {
-	js.Global().Set("goEchoString", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
-		if len(args) < 1 {
-			return "No parameters provided"
-		}
-		input := args[0].String()
-		fmt.Println("Received string from JS:", input)
-		return input
-	}))
-}
-
 func registerFuncs() {
 	js.Global().Set("format", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
 		if len(args) < 1 {
 			return "Database URL parameter required"
 		}
 		dbUrl := args[0].String()
-		logToJS("Starting format: " + dbUrl)
 
-		// 尝试格式化操作
 		err := wasm.Format(dbUrl)
 		if err != nil {
 			errMsg := fmt.Sprintf("Format failed: %v", err)
@@ -40,13 +26,12 @@ func registerFuncs() {
 		}
 
 		logToJS("Format completed successfully")
-		return "Format completed"
+		return nil
 	}))
 }
 
 func main() {
-	registerStringEcho()
-	registerFuncs() 
+	registerFuncs()
 
 	logToJS("JuiceFS WebAssembly module initialized")
 
